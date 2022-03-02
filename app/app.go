6f30f7a03c3d4780b84bd62bb3654420007c6841
fajarabdillahfn <fajar.abdillahfn@gmail.com>
2022-03-02 16:53:36 +0700

    add sanity check and move variables to env

--- app/app.go
+++ app/app.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/fajarabdillahfn/banking-lib/logger"
@@ -15,8 +16,25 @@ import (
 	_ "github.com/lib/pq"
 )
 
+func sanityCheck() {
+	envProps := []string{
+		"SERVER_ADDRESS",
+		"SERVER_PORT",
+		"DB_USER",
+		"DB_PASSWD",
+		"DB_ADDR",
+		"DB_PORT",
+		"DB_NAME",
+	}
+	for _, k := range envProps {
+		if os.Getenv(k) == "" {
+			logger.Fatal(fmt.Sprintf("Environment variable %s not defined. Terminating application...", k))
+		}
+	}
+}
+
 func Start() {
-	// sanityCheck()
+	sanityCheck()
 	router := mux.NewRouter()
 	authRepository := domain.NewAuthRepositoryDb(getDbClient())
 	ah := AuthHandler{service: service.NewLoginService(authRepository, domain.GetRolePermissions())}
@@ -25,14 +43,23 @@ func Start() {
 	router.HandleFunc("/auth/refresh", ah.Refresh).Methods(http.MethodPost)
 	router.HandleFunc("/auth/verify", ah.Verify).Methods(http.MethodGet)
 
-	address := "localhost"
-	port := "8181"
-	logger.Info(fmt.Sprintf("Starting OAuth server on %s:%s ...", address, port))
+	// starting server
+	address := os.Getenv("SERVER_ADDRESS")
+	port := os.Getenv("SERVER_PORT")
+	logger.Info(fmt.Sprintf("Starting server on %s:%s ...", address, port))
 	log.Fatal(http.ListenAndServe(fmt.Sprintf("%s:%s", address, port), router))
 }
 
 func getDbClient() *sqlx.DB {
-	client, err := sqlx.Open("postgres", "postgres://abdillah.fajar:masBed0311@localhost/banking_app?sslmode=disable")
+	dbDriver := os.Getenv("DB_DRIVER")
+	dbUser := os.Getenv("DB_USER")
+	dbPasswd := os.Getenv("DB_PASSWD")
+	dbAddr := os.Getenv("DB_ADDR")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+
+	dataSource := fmt.Sprintf("%s://%s:%s@%s:%s/%s?sslmode=disable", dbDriver, dbUser, dbPasswd, dbAddr, dbPort, dbName)
+	client, err := sqlx.Open(dbDriver, dataSource)
 	if err != nil {
 		panic(err)
 	}
